7a3214a68a8d52f076e270986d28e8b508b6d903
Rajasekar D <11868287+rajasekar-d@users.noreply.github.com>
2021-05-16 20:48:47 +0530

    fix(): use the right command for windows Closes #11

--- schematics/cap-init/index.ts
+++ schematics/cap-init/index.ts
@@ -5,6 +5,11 @@ import { Observable } from 'rxjs';
 export default function({ command, args }: { command: string; args: string[]; }) {
   return (host: Tree, _context: SchematicContext) => {
     return new Observable<Tree>(subscriber => {
+      if (command === 'npx') {
+        command = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+      } else if (command === 'npm') {
+        command = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+      }
       const cp = spawn(command, args, { stdio: 'inherit' });
       cp.on('error', error => {
         subscriber.error(error);
